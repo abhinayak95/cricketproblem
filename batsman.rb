2607042bf2a5b74e85f9366538c4e7415c18068f
abhinayak <abhishek.nayak@mountblue.io>
2018-11-03 16:16:51 +0530

    Added Batsman class

--- batsman.rb
+++ batsman.rb
@@ -1,14 +1,23 @@
 class Batsman
-  attr_accessor :name, :runs, :balls_faced, :is_out, : batsmen_probability
-  def initialize(name)
-
+  attr_accessor :name, :runs, :balls_faced, :is_out, :probability
+  def initialize(name, probability)
+    @name = name
+    @probability = probability
+    @runs = 0
+    @balls_faced = 0
   end
-  def self.add_runs(runs)
+  def add_runs(runs)
     @runs += runs
     @balls_faced += 1
   end
   def set_status(status)
     @is_out = true
     @balls_faced += 1
+  end
+  def get_probability()
+    return @probability
+  end
+  def get_scorecard
+    return "#{runs} #{balls_faced}"
   end
 end
--- cricket_problem.rb
+++ cricket_problem.rb
@@ -1,31 +1,17 @@
+require_relative "batsman.rb"
 
+batsman1 = Batsman.new('Kirat Boli', {0 => 5, 1 => 30, 2 => 25, 3 => 10, 4 => 15, 5 => 1, 6 => 9, 7 => 5})
+batsman2 = Batsman.new('NS Nodhi', {0 => 10, 1 => 40, 2 => 20, 3 => 5, 4 => 10, 5 => 1, 6 => 4, 7 => 10})
+batsman3 = Batsman.new('R Rumrah', {0 => 20, 1 => 30, 2 => 15, 3 => 5, 4 => 5, 5 => 1, 6 => 4, 7 => 20})
+batsman4 = Batsman.new('Shashi Henra', {0 => 30, 1 => 25, 2 => 10, 3 => 0, 4 => 5, 5 => 1, 6 => 4, 7 => 30})
 
-
-ball_result = [0, 1, 2, 3, 4, 5, 6, 7]
-batsmen = ['Kirat Boli', 'NS Nodhi', 'R Rumrah', 'Shashi Henra']
+batsmen = [batsman1, batsman2, batsman3, batsman4]
 next_bastman = 2
-playing_batsmen = ['Kirat Boli', 'NS Nodhi']
+playing_batsmen = [batsman1, batsman2]
 on_strike = 0
 balls_remaining = 24
 wickets_remaining = 3
-score_card = {
-  'Kirat Boli' => [0, 0],
-  'NS Nodhi' => [0, 0],
-  'R Rumrah' => [0, 0],
-  'Shashi Henra' => [0, 0]
-}
-
-batsmen_probability = {
-  'Kirat Boli' => {0 => 5, 1 => 30, 2 => 25, 3 => 10, 4 => 15, 5 => 1, 6 => 9, 7 => 5},
-  'NS Nodhi' => {0 => 5, 1 => 30, 2 => 25, 3 => 10, 4 => 15, 5 => 1, 6 => 9, 7 => 5},
-  'R Rumrah' => {0 => 5, 1 => 30, 2 => 25, 3 => 10, 4 => 15, 5 => 1, 6 => 9, 7 => 5},
-  'Shashi Henra' => {0 => 5, 1 => 30, 2 => 25, 3 => 10, 4 => 15, 5 => 1, 6 => 9, 7 => 5},
-}
-
-# Kirat Boli    5%        30%   25%   10%   15%   1%    9%    5%
-# NS Nodhi      10%       40%   20%   5%    10%   1%    4%    10%
-# R Rumrah      20%       30%   15%   5%    5%    1%    4%    20%
-# Shashi Henra  30%       25%   10%   0%    5%    1%    4%    30%
+runs_scored = 0
 
 def random_weighted(weighted)
   max    = sum_of_weights(weighted)
@@ -42,28 +28,43 @@ end
 
 def change_strike
   on_strike = (on_strike.to_i + 1) % 2
-  puts on_strike
+  # puts on_strike
 end
 
-for i in 1..40
+24.times do |i|
+  balls_remaining -= 1
   if i % 6 == 0
     change_strike()
   end
   # random generator
-  puts playing_batsmen[on_strike]
-  result = random_weighted(batsmen_probability[playing_batsmen[on_strike]])
+  # puts playing_batsmen[on_strike]
+  batsman =batsman1
+  # puts 'this' + batsman.to_s
+  result = random_weighted(batsman.get_probability())
   if(result == 7)
     playing_batsmen.delete_at(on_strike)
     playing_batsmen.insert(on_strike,batsmen[next_bastman])
     next_bastman += 1
+    wickets_remaining -= 1
+    break if wickets_remaining == 0
   elsif(result == 1 || result == 3 || result == 5)
-    score_card[playing_batsmen[on_strike]][0] += result
-    score_card[playing_batsmen[on_strike]][1] += 1
+    playing_batsmen[on_strike].add_runs(result)
+    runs_scored += result
     change_strike()
+    break if runs_scored >= 40
   else
-    score_card[playing_batsmen[on_strike]][0] += result
-    score_card[playing_batsmen[on_strike]][1] += 1
+    playing_batsmen[on_strike].add_runs(result)
+    runs_scored += result
+    break if runs_scored >= 40
   end
 end
 
-puts score_card.to_s
+if runs_scored - 40 >= 0
+  puts "India won by #{wickets_remaining} wickets and #{balls_remaining} balls remaining"
+else
+  puts "India lost by #{40 - runs_scored} runs"
+end
+batsmen.each{
+  |batsman|
+  puts batsman.name + ' ' + batsman.get_scorecard()
+}
